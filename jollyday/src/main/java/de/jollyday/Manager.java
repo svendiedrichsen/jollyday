d4270b35702353b632d47dbb9837c585f04f36d0
diedrichsen <diedrichsen@e7149cbe-73de-4594-858e-8f9d7d5835ab>
2010-01-18 23:31:24 +0000

    little changes

--- jollyday/src/main/java/de/jollyday/Manager.java
+++ jollyday/src/main/java/de/jollyday/Manager.java
@@ -3,26 +3,20 @@ package de.jollyday;
 import java.util.Calendar;
 import java.util.Set;
 
-public class Manager {
+import de.jollyday.util.CalendarUtil;
+
+public abstract class Manager {
 
 	private static Manager MANAGER = null;
 
-	private Manager() {
-	}
-
 	public static final Manager getInstance() {
 		if (null == MANAGER) {
-			MANAGER = new Manager();
 		}
 		return MANAGER;
 	}
 	
 	public boolean isHoliday(Calendar c){
-		Calendar cal = (Calendar)c.clone();
-		cal.clear(Calendar.HOUR);
-		cal.clear(Calendar.MINUTE);
-		cal.clear(Calendar.SECOND);
-		cal.clear(Calendar.MILLISECOND);
+		Calendar cal = CalendarUtil.truncate((Calendar)c.clone());
 		int year = cal.get(Calendar.YEAR);
 		return getHolidays(year).contains(cal);
 	}
---
+++ jollyday/src/main/java/de/jollyday/util/CalendarUtil.java
@@ -0,0 +1,19 @@
+package de.jollyday.util;
+
+import java.util.Calendar;
+
+public abstract class CalendarUtil {
+
+	public static Calendar truncate(Calendar c) {
+		c.clear(Calendar.HOUR);
+		c.clear(Calendar.MINUTE);
+		c.clear(Calendar.SECOND);
+		c.clear(Calendar.MILLISECOND);
+		return c;
+	}
+
+	public static Calendar create() {
+		return truncate(Calendar.getInstance());
+	}
+
+}
