f681477ea4f8ea8602435894ffe6a3b7158ba82e
diedrichsen <diedrichsen@e7149cbe-73de-4594-858e-8f9d7d5835ab>
2010-01-27 20:56:43 +0000

    Changed manager impl app configuration

--- jollyday/src/main/java/de/jollyday/Manager.java
+++ jollyday/src/main/java/de/jollyday/Manager.java
@@ -7,28 +7,34 @@ import java.util.Set;
 import org.joda.time.LocalDate;
 
 public abstract class Manager {
-	
-	private static final String MANAGER_IMPL_CLASS_PREFIX = "manager.impl.";
+
+	private static final String MANAGER_IMPL_CLASS_PREFIX = "manager.impl";
 	private static final String CONFIG_FILE = "application.properties";
 
-	public static final Manager getInstance(String country) throws Exception{
+	public static final Manager getInstance(String country) throws Exception {
 		Properties props = new Properties();
 		props.load(ClassLoader.getSystemResourceAsStream(CONFIG_FILE));
-		Manager m = (Manager)Class.forName(props.getProperty(MANAGER_IMPL_CLASS_PREFIX+country)).newInstance();
+		String managerImplClass = null;
+		if (props.stringPropertyNames().contains(MANAGER_IMPL_CLASS_PREFIX + "." + country)) {
+			managerImplClass = props.getProperty(MANAGER_IMPL_CLASS_PREFIX + "." + country);
+		} else {
+			managerImplClass = props.getProperty(MANAGER_IMPL_CLASS_PREFIX);
+		}
+		Manager m = (Manager) Class.forName(managerImplClass).newInstance();
 		m.init(country);
 		return m;
 	}
-	
-	public boolean isHoliday(Calendar c, String...args){
+
+	public boolean isHoliday(Calendar c, String... args) {
 		return isHoliday(new LocalDate(c), args);
 	}
-	
-	public boolean isHoliday(LocalDate c, String...args){
+
+	public boolean isHoliday(LocalDate c, String... args) {
 		return getHolidays(c.getYear(), args).contains(c);
 	}
-	
-	abstract public Set<LocalDate> getHolidays(int year, String...args);
-	
+
+	abstract public Set<LocalDate> getHolidays(int year, String... args);
+
 	abstract public void init(String country) throws Exception;
-	
+
 }
