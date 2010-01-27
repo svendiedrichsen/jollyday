49ee2374cf8fcb5f3bcd953229c871fad272e4ef
diedrichsen <diedrichsen@e7149cbe-73de-4594-858e-8f9d7d5835ab>
2010-01-27 21:16:31 +0000

    added comments

--- jollyday/src/main/java/de/jollyday/impl/XMLManager.java
+++ jollyday/src/main/java/de/jollyday/impl/XMLManager.java
@@ -24,40 +24,51 @@ import de.jollyday.parser.impl.FixedWeekdayInMonthParser;
 import de.jollyday.parser.impl.RelativeToEasternParser;
 import de.jollyday.parser.impl.RelativeToFixedParser;
 
+/**
+ * Manager implementation for reading data from XML files. The files
+ * with the name pattern Holidays_[country].xml will be read from
+ * the system classpath. It uses a list a parsers for parsing the
+ * different type of XML nodes.
+ * @author Sven Diedrichsen
+ *
+ */
 public class XMLManager extends Manager {
 
-	private static final Logger LOG = Logger.getLogger(XMLManager.class.getName());
+	private static final Logger LOG = Logger.getLogger(XMLManager.class
+			.getName());
 	private static final String PACKAGE = "de.jollyday.config";
 	private static final String FILE_PREFIX = "Holidays";
 	private static final String FILE_SUFFIX = ".xml";
 
 	private static final Collection<HolidayParser> PARSER = new HashSet<HolidayParser>();
-	
+
 	private Configuration configuration;
-	
-	static{
+
+	static {
 		PARSER.add(new FixedWeekdayInMonthParser());
 		PARSER.add(new FixedParser());
 		PARSER.add(new FixedMovingParser());
 		PARSER.add(new RelativeToEasternParser());
 		PARSER.add(new RelativeToFixedParser());
 	}
-	
+
 	@Override
 	public Set<LocalDate> getHolidays(int year, String... args) {
 		return getHolidays(year, configuration, args);
 	}
-	
-	private Set<LocalDate> getHolidays(int year, Configuration c, String... args) {
+
+	private Set<LocalDate> getHolidays(int year, Configuration c,
+			String... args) {
 		Set<LocalDate> holidaySet = new HashSet<LocalDate>();
-		if(LOG.isLoggable(Level.FINER)){
-			LOG.finer("Adding holidays for "+c.getDescription());
+		if (LOG.isLoggable(Level.FINER)) {
+			LOG.finer("Adding holidays for " + c.getDescription());
 		}
 		parseHolidays(year, holidaySet, c.getHolidays());
-		if(args != null && args.length > 0){
-			for(Configuration config : c.getSubConfigurations()){
-				if(args[0].equalsIgnoreCase(config.getHierarchy())){
-					holidaySet.addAll(getHolidays(year, config, Arrays.copyOfRange(args, 1, args.length)));
+		if (args != null && args.length > 0) {
+			for (Configuration config : c.getSubConfigurations()) {
+				if (args[0].equalsIgnoreCase(config.getHierarchy())) {
+					holidaySet.addAll(getHolidays(year, config, Arrays
+							.copyOfRange(args, 1, args.length)));
 					break;
 				}
 			}
@@ -65,25 +76,31 @@ public class XMLManager extends Manager {
 		return holidaySet;
 	}
 
-	private void parseHolidays(int year, Set<LocalDate> holidays, Holidays config) {
-		for(HolidayParser p : PARSER){
+	private void parseHolidays(int year, Set<LocalDate> holidays,
+			Holidays config) {
+		for (HolidayParser p : PARSER) {
 			p.parse(year, holidays, config);
 		}
 	}
 
 	@Override
 	public void init(String country) throws Exception {
-		InputStream stream = ClassLoader.getSystemResourceAsStream(FILE_PREFIX + "_"
-				+ country + FILE_SUFFIX);
-		JAXBContext ctx = JAXBContext.newInstance(PACKAGE);
-		Unmarshaller um = ctx.createUnmarshaller();
-		JAXBElement<Configuration> el = (JAXBElement<Configuration>) um.unmarshal(stream);
-		configuration = el.getValue();
-		stream.close();
-		if(LOG.isLoggable(Level.FINER)){
-			LOG.finer("Found configuration for "+configuration.getDescription());
-			for(Configuration c : configuration.getSubConfigurations()){
-				LOG.finer("Sub-configuration "+c.getDescription()+"("+c.getHierarchy()+").");
+		InputStream stream = ClassLoader.getSystemResourceAsStream(FILE_PREFIX
+				+ "_" + country + FILE_SUFFIX);
+		try {
+			JAXBContext ctx = JAXBContext.newInstance(PACKAGE);
+			Unmarshaller um = ctx.createUnmarshaller();
+			JAXBElement<Configuration> el = (JAXBElement<Configuration>) um.unmarshal(stream);
+			configuration = el.getValue();
+		} finally {
+			stream.close();
+		}
+		if (LOG.isLoggable(Level.FINER)) {
+			LOG.finer("Found configuration for "
+					+ configuration.getDescription());
+			for (Configuration c : configuration.getSubConfigurations()) {
+				LOG.finer("Sub-configuration " + c.getDescription() + "("
+						+ c.getHierarchy() + ").");
 			}
 		}
 	}
