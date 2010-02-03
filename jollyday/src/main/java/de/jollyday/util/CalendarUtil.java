4df49a6016e65559b2b797998a719909ad26fd7e
diedrichsen <diedrichsen@e7149cbe-73de-4594-858e-8f9d7d5835ab>
2010-02-03 15:17:38 +0000

    split easter calculation into julian and gregorian

--- jollyday/src/main/java/de/jollyday/util/CalendarUtil.java
+++ jollyday/src/main/java/de/jollyday/util/CalendarUtil.java
@@ -83,38 +83,59 @@ public abstract class CalendarUtil {
 	 * @return Easter sunday.
 	 */
 	public static LocalDate getEasterSunday(int year){
-		int a,b,c,d,e,f,g,h,i,j,k,l;
-		int x,month,day;
 		if (year <= 1583) 
 		{   
-			a = year%4;
-			b = year%7;
-			c = year%19;
-			d = (19*c+15)%30;
-			e = (2*a+4*b-d+34)%7;
-			x = d+e+114;
-			month = x/31;
-			day = (x%31)+1;
+			return getJulianEasterSunday(year);
 		}	
 		else
 		{   
-	 		a = year%19;
-			b = year/100;
-			c = year%100;
-			d = b/4;
-			e = b%4;
-			f = (b+8)/25;
-			g = (b-f+1)/3;
-			h = (19*a+b-d-g+15)%30;
-			i = c/4;
-			j = c%4;
-			k = (32+2*e+2*i-h-j)%7;
-			l = (a+11*h+22*k)/451;
-			x = h+k-7*l+114;
-			month = x/31;
-			day = (x%31)+1;
+			return getGregorianEasterSunday(year);
 		}
-		return create(year, (month == 3 ? DateTimeConstants.MARCH : DateTimeConstants.APRIL), day);
+	}
+	
+	/**
+	 * Returns the easter sunday within the julian chronology.
+	 * @param year
+	 * @return julian easter sunday
+	 */
+	public static LocalDate getJulianEasterSunday(int year){
+		int a,b,c,d,e;
+		int x,month,day;
+		a = year%4;
+		b = year%7;
+		c = year%19;
+		d = (19*c+15)%30;
+		e = (2*a+4*b-d+34)%7;
+		x = d+e+114;
+		month = x/31;
+		day = (x%31)+1;
+		return create(year, (month == 3 ? DateTimeConstants.MARCH : DateTimeConstants.APRIL), day, JulianChronology.getInstance());
+	}
+	
+	/**
+	 * Returns the easter sunday within the gregorian chronology.
+	 * @param year
+	 * @return gregorian easter sunday.
+	 */
+	public static LocalDate getGregorianEasterSunday(int year){
+		int a,b,c,d,e,f,g,h,i,j,k,l;
+		int x,month,day;
+		a = year%19;
+		b = year/100;
+		c = year%100;
+		d = b/4;
+		e = b%4;
+		f = (b+8)/25;
+		g = (b-f+1)/3;
+		h = (19*a+b-d-g+15)%30;
+		i = c/4;
+		j = c%4;
+		k = (32+2*e+2*i-h-j)%7;
+		l = (a+11*h+22*k)/451;
+		x = h+k-7*l+114;
+		month = x/31;
+		day = (x%31)+1;
+		return create(year, (month == 3 ? DateTimeConstants.MARCH : DateTimeConstants.APRIL), day, GregorianChronology.getInstance());
 	}
 
 	/**
