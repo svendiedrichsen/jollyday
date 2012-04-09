e30999ec1dcf3c5cc347f1ede07268f311fc6d2e
diedrichsen <diedrichsen@e7149cbe-73de-4594-858e-8f9d7d5835ab>
2012-04-09 22:49:36 +0000

    added fixed processor test. added mockito test dependency.

---
+++ branches/v_0_5_0/src/test/java/de/jollyday/processor/impl/FixedProcessorTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2012 Sven Diedrichsen 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * express or implied. See the License for the specific language 
+ * governing permissions and limitations under the License. 
+ */
+package de.jollyday.processor.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import de.jollyday.config.Fixed;
+
+/**
+ * @author sven
+ * 
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class FixedProcessorTest {
+
+	@Mock
+	private Fixed fixed;
+	private FixedProcessor fixedProcessor;
+
+	@Before
+	public void setup() throws Exception {
+		fixedProcessor = new FixedProcessor(fixed);
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testConstructorNullCheck() throws Exception {
+		new FixedProcessor(null);
+	}
+
+}
