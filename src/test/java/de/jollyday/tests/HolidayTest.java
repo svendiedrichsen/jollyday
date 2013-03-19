397bada5fd8691243ab40586c3909b9f6c833a7d
diedrichsen <diedrichsen@e7149cbe-73de-4594-858e-8f9d7d5835ab>
2013-03-19 00:45:03 +0000

    Fixed potential multi-thread vulnerability with holidays caching. Extended test to run multithreaded.

--- src/test/java/de/jollyday/tests/HolidayTest.java
+++ src/test/java/de/jollyday/tests/HolidayTest.java
@@ -22,6 +22,12 @@ import java.util.HashSet;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -47,7 +53,8 @@ import de.jollyday.util.CalendarUtil;
  */
 public class HolidayTest {
 
-	private final static Logger LOG = Logger.getLogger(HolidayTest.class.getName());
+	private final static Logger LOG = Logger.getLogger(HolidayTest.class
+			.getName());
 
 	private static final Set<LocalDate> test_days = new HashSet<LocalDate>();
 	private static final Set<LocalDate> test_days_l1 = new HashSet<LocalDate>();
@@ -57,37 +64,52 @@ public class HolidayTest {
 	private static CalendarUtil calendarUtil = new CalendarUtil();
 
 	static {
-		test_days.add(calendarUtil.create(2010, DateTimeConstants.FEBRUARY, 17));
+		test_days
+				.add(calendarUtil.create(2010, DateTimeConstants.FEBRUARY, 17));
 		test_days.add(calendarUtil.create(2010, DateTimeConstants.AUGUST, 30));
 		test_days.add(calendarUtil.create(2010, DateTimeConstants.APRIL, 2));
 		test_days.add(calendarUtil.create(2010, DateTimeConstants.APRIL, 5));
-		test_days.add(calendarUtil.create(2010, DateTimeConstants.NOVEMBER, 17));
-		test_days.add(calendarUtil.create(2010, DateTimeConstants.NOVEMBER, 28));
+		test_days
+				.add(calendarUtil.create(2010, DateTimeConstants.NOVEMBER, 17));
+		test_days
+				.add(calendarUtil.create(2010, DateTimeConstants.NOVEMBER, 28));
 		test_days.add(calendarUtil.create(2010, DateTimeConstants.JANUARY, 1));
 		test_days.add(calendarUtil.create(2010, DateTimeConstants.JANUARY, 18));
-		test_days.add(calendarUtil.create(2010, DateTimeConstants.NOVEMBER, 26));
+		test_days
+				.add(calendarUtil.create(2010, DateTimeConstants.NOVEMBER, 26));
 		test_days_l1.addAll(test_days);
-		test_days_l1.add(calendarUtil.create(2010, DateTimeConstants.JANUARY, 2));
+		test_days_l1.add(calendarUtil
+				.create(2010, DateTimeConstants.JANUARY, 2));
 		test_days_l2.addAll(test_days_l1);
-		test_days_l2.add(calendarUtil.create(2010, DateTimeConstants.JANUARY, 3));
+		test_days_l2.add(calendarUtil
+				.create(2010, DateTimeConstants.JANUARY, 3));
 
 		test_days_l11.addAll(test_days);
-		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.JULY, 27));
+		test_days_l11
+				.add(calendarUtil.create(2010, DateTimeConstants.JULY, 27));
 		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.JULY, 9));
-		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.FEBRUARY, 26));
-		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.AUGUST, 11));
-		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.SEPTEMBER, 6));
-		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.SEPTEMBER, 10));
-		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.NOVEMBER, 17));
-		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.DECEMBER, 8));
-		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.DECEMBER, 17));
+		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.FEBRUARY,
+				26));
+		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.AUGUST,
+				11));
+		test_days_l11.add(calendarUtil.create(2010,
+				DateTimeConstants.SEPTEMBER, 6));
+		test_days_l11.add(calendarUtil.create(2010,
+				DateTimeConstants.SEPTEMBER, 10));
+		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.NOVEMBER,
+				17));
+		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.DECEMBER,
+				8));
+		test_days_l11.add(calendarUtil.create(2010, DateTimeConstants.DECEMBER,
+				17));
 	}
 
 	private Locale defaultLocale;
 
 	@Before
 	public void init() {
-		System.setProperty("de.jollyday.config.urls", "file:./src/test/resources/test.app.properties");
+		System.setProperty("de.jollyday.config.urls",
+				"file:./src/test/resources/test.app.properties");
 		defaultLocale = Locale.getDefault();
 		Locale.setDefault(Locale.GERMAN);
 	}
@@ -108,12 +130,17 @@ public class HolidayTest {
 		HolidayManager m = HolidayManager.getInstance("test");
 		CalendarHierarchy h = m.getCalendarHierarchy();
 		Assert.assertEquals("Wrong id.", "test", h.getId());
-		Assert.assertEquals("Wrong number of children on first level.", 2, h.getChildren().size());
+		Assert.assertEquals("Wrong number of children on first level.", 2, h
+				.getChildren().size());
 		for (CalendarHierarchy hi : h.getChildren().values()) {
 			if (hi.getId().equalsIgnoreCase("level1")) {
-				Assert.assertEquals("Wrong number of children on second level of level 1.", 1, hi.getChildren().size());
+				Assert.assertEquals(
+						"Wrong number of children on second level of level 1.",
+						1, hi.getChildren().size());
 			} else if (hi.getId().equalsIgnoreCase("level11")) {
-				Assert.assertEquals("Wrong number of children on second level of level 11.", 0, hi.getChildren().size());
+				Assert.assertEquals(
+						"Wrong number of children on second level of level 11.",
+						0, hi.getChildren().size());
 			}
 		}
 	}
@@ -126,30 +153,44 @@ public class HolidayTest {
 		}
 	}
 
-	private void assertNotUndefined(HolidayCalendar calendar, CalendarHierarchy c) {
-		assertFalse("Calendar " + calendar + " Hierarchy " + c.getId() + " is lacking a description.",
+	private void assertNotUndefined(HolidayCalendar calendar,
+			CalendarHierarchy c) {
+		assertFalse("Calendar " + calendar + " Hierarchy " + c.getId()
+				+ " is lacking a description.",
 				"undefined".equals(c.getDescription()));
-		for (Map.Entry<String, CalendarHierarchy> element : c.getChildren().entrySet()) {
+		for (Map.Entry<String, CalendarHierarchy> element : c.getChildren()
+				.entrySet()) {
 			assertNotUndefined(calendar, element.getValue());
 		}
 	}
 
 	@Test
-	public void testIsHolidayPerformance() throws Exception {
+	public void testIsHolidayPerformanceMultithreaded() throws Exception {
 		LocalDate date = calendarUtil.create(2010, 1, 1);
-		int count = 0;
-		long sumDuration = 0;
-		while (date.getYear() < 2011) {
-			long start = System.currentTimeMillis();
-			HolidayManager m = HolidayManager.getInstance("test");
-			m.isHoliday(date);
-			long duration = System.currentTimeMillis() - start;
+		final AtomicLong count = new AtomicLong(0);
+		final AtomicLong sumDuration = new AtomicLong(0);
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		while (date.getYear() < 2013) {
+			final LocalDate localDate = date;
+			executorService.submit(new Runnable() {
+				public void run() {
+					long start = System.currentTimeMillis();
+					HolidayManager m = HolidayManager.getInstance("test");
+					m.isHoliday(localDate);
+					long duration = System.currentTimeMillis() - start;
+					duration = System.currentTimeMillis() - start;
+					count.incrementAndGet();
+					sumDuration.addAndGet(duration);
+				}
+			});
 			date = date.plusDays(1);
-			duration = System.currentTimeMillis() - start;
-			count++;
-			sumDuration += duration;
 		}
-		LOG.log(Level.INFO, "isHoliday took " + sumDuration / count + " millis average.");
+		executorService.shutdown();
+		executorService.awaitTermination(5, TimeUnit.SECONDS);
+		LOG.log(Level.INFO,
+				"isHoliday took " + sumDuration.doubleValue()
+						/ count.doubleValue() + " millis average tested with "
+						+ count.longValue() + " calls.");
 	}
 
 	@Test
@@ -170,7 +211,8 @@ public class HolidayTest {
 		HolidayManager m = HolidayManager.getInstance("test");
 		Set<Holiday> holidays = m.getHolidays(2010);
 		for (Holiday d : holidays) {
-			Assert.assertEquals("Wrong chronology.", isoChrono, d.getDate().getChronology());
+			Assert.assertEquals("Wrong chronology.", isoChrono, d.getDate()
+					.getChronology());
 		}
 	}
 
@@ -183,7 +225,8 @@ public class HolidayTest {
 	}
 
 	private void assertDates(Set<LocalDate> expected, Set<Holiday> holidays) {
-		Assert.assertEquals("Wrong number of dates.", expected.size(), holidays.size());
+		Assert.assertEquals("Wrong number of dates.", expected.size(),
+				holidays.size());
 		for (LocalDate d : expected) {
 			if (!calendarUtil.contains(holidays, d)) {
 				Assert.fail("Missing " + d + " in " + holidays);
@@ -222,35 +265,45 @@ public class HolidayTest {
 
 	@Test
 	public void testAllAvailableManagers() throws Exception {
-		Set<String> supportedCalendarCodes = HolidayManager.getSupportedCalendarCodes();
+		Set<String> supportedCalendarCodes = HolidayManager
+				.getSupportedCalendarCodes();
 		Assert.assertNotNull(supportedCalendarCodes);
 		Assert.assertFalse(supportedCalendarCodes.isEmpty());
 		for (String calendar : supportedCalendarCodes) {
 			HolidayManager manager = HolidayManager.getInstance(calendar);
-			Assert.assertNotNull("Manager for calendar " + calendar + " is NULL.", manager);
+			Assert.assertNotNull("Manager for calendar " + calendar
+					+ " is NULL.", manager);
 		}
 	}
 
 	@Test
 	public void testHolidayDescription() {
-		Holiday h = new Holiday(calendarUtil.create(2011, 2, 2), "CHRISTMAS", LocalizedHolidayType.OFFICIAL_HOLIDAY);
-		Assert.assertEquals("Wrong description", "Weihnachten", h.getDescription());
-		Assert.assertEquals("Wrong description", "Christmas", h.getDescription(Locale.ENGLISH));
-		Assert.assertEquals("Wrong description", "Kerstmis", h.getDescription(new Locale("nl")));
+		Holiday h = new Holiday(calendarUtil.create(2011, 2, 2), "CHRISTMAS",
+				LocalizedHolidayType.OFFICIAL_HOLIDAY);
+		Assert.assertEquals("Wrong description", "Weihnachten",
+				h.getDescription());
+		Assert.assertEquals("Wrong description", "Christmas",
+				h.getDescription(Locale.ENGLISH));
+		Assert.assertEquals("Wrong description", "Kerstmis",
+				h.getDescription(new Locale("nl")));
 	}
 
 	@Test
 	public void testHolidayEquals() {
-		Holiday h1 = new Holiday(calendarUtil.create(2011, 2, 2), "CHRISTMAS", LocalizedHolidayType.OFFICIAL_HOLIDAY);
-		Assert.assertTrue("Wrong equals implementation", h1.equals(h1));
-		Holiday h2b = new Holiday(calendarUtil.create(2011, 2, 2), new String("CHRISTMAS"),
+		Holiday h1 = new Holiday(calendarUtil.create(2011, 2, 2), "CHRISTMAS",
 				LocalizedHolidayType.OFFICIAL_HOLIDAY);
+		Assert.assertTrue("Wrong equals implementation", h1.equals(h1));
+		Holiday h2b = new Holiday(calendarUtil.create(2011, 2, 2), new String(
+				"CHRISTMAS"), LocalizedHolidayType.OFFICIAL_HOLIDAY);
 		Assert.assertTrue("Wrong equals implementation", h1.equals(h2b));
-		Holiday h2 = new Holiday(calendarUtil.create(2011, 2, 1), "CHRISTMAS", LocalizedHolidayType.OFFICIAL_HOLIDAY);
+		Holiday h2 = new Holiday(calendarUtil.create(2011, 2, 1), "CHRISTMAS",
+				LocalizedHolidayType.OFFICIAL_HOLIDAY);
 		Assert.assertFalse("Wrong equals implementation", h1.equals(h2));
-		Holiday h3 = new Holiday(calendarUtil.create(2011, 2, 2), "NEW_YEAR", LocalizedHolidayType.OFFICIAL_HOLIDAY);
+		Holiday h3 = new Holiday(calendarUtil.create(2011, 2, 2), "NEW_YEAR",
+				LocalizedHolidayType.OFFICIAL_HOLIDAY);
 		Assert.assertFalse("Wrong equals implementation", h1.equals(h3));
-		Holiday h4 = new Holiday(calendarUtil.create(2011, 2, 2), "CHRISTMAS", LocalizedHolidayType.UNOFFICIAL_HOLIDAY);
+		Holiday h4 = new Holiday(calendarUtil.create(2011, 2, 2), "CHRISTMAS",
+				LocalizedHolidayType.UNOFFICIAL_HOLIDAY);
 		Assert.assertFalse("Wrong equals implementation", h1.equals(h4));
 	}
 
