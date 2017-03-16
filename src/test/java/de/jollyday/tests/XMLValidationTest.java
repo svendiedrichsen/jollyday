f63015ed48060921dc5a57674a003d8636fd212d
Sven Diedrichsen <sdiedrichsen@yahoo.de>
2017-03-16 15:18:13 +0100

    added xmlunit test to validate all holiday xml files against schema. found invalid xml file and fixed it.

--- src/test/java/de/jollyday/tests/XMLValidationTest.java
+++ src/test/java/de/jollyday/tests/XMLValidationTest.java
@@ -8,29 +8,46 @@ import org.xmlunit.validation.Validator;
 
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.assertTrue;
 
-/**
- * @author sdiedrichsen
- * @version $Id$
- * @since 16.03.17
- */
 public class XMLValidationTest {
 
     private File schemaFile;
+    private Validator validator;
 
     @Before
     public void setUp() throws Exception {
         schemaFile = new File("src/main/xsd/Holiday.xsd");
+        validator = Validator.forLanguage(Languages.W3C_XML_SCHEMA_NS_URI);
+        validator.setSchemaSource(new StreamSource(schemaFile));
     }
 
     @Test
     public void testSchemaIsValid() throws Exception {
-        final Validator validator = Validator.forLanguage(Languages.W3C_XML_SCHEMA_NS_URI);
-        validator.setSchemaSource(new StreamSource(schemaFile));
+        assertTrue("Schema file "+schemaFile+" does not exist.", schemaFile.exists());
         final ValidationResult validationResult = validator.validateSchema();
         assertTrue("Schema '"+ schemaFile +"' is not valid: "+validationResult.getProblems(), validationResult.isValid());
     }
 
+    @Test
+    public void testHolidayFilesAreValid()throws Exception {
+        final Path holidaysFolder = Paths.get("src/main/resources/holidays/");
+        Files.list(holidaysFolder).forEach(path -> validateHolidayFile(path));
+    }
+
+    private void validateHolidayFile(Path path){
+
+        try {
+            final ValidationResult validationResult = validator.validateInstance(new StreamSource(new FileInputStream(path.toFile())));
+            assertTrue("Validation of holiday file '"+path+" failed: "+validationResult.getProblems(), validationResult.isValid());
+        } catch (FileNotFoundException e) {
+            throw new IllegalStateException("Cannot validate holiday file "+path);
+        }
+    }
 }
